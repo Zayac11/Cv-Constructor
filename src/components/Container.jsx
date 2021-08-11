c2cb7216f915659c39884db8dc9bd12e03e0fc27
Zayac11 <brunogrbstudios@gmail.com>
2021-08-11 18:59:24 +0300

    first commit

---
+++ src/components/Container.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import s from './Container.module.scss'
+
+const Container = () => {
+    return (
+        <div className='outer'>
+            <div className='container'>
+
+            </div>
+        </div>
+    )
+}
+
+export default Container
